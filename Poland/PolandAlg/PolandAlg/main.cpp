b072a48ab7e03c6a43e0f80fe7acf591bc7a66a7
DSereda <dsereda2017@ukr.net>
2021-11-03 20:42:02 +0200

    update2

--- Poland/PolandAlg/PolandAlg/main.cpp
+++ Poland/PolandAlg/PolandAlg/main.cpp
@@ -1,124 +1,156 @@
 #include <iostream>
 #include <string>
 #include <stack>
+#include <vector>
 using namespace std;
 
+vector<string> strToVector(const string& str)
+{
+    vector<string> temp(0);
+    string s;
 
-string toPoland(const string& str) {
-    string res;
-    stack<char> st;
-
-    for (const char ch : str) {
-        if (isdigit(ch)) {
-            res += ch;
+    for (int i = 0; i < str.size(); i++)
+    {
+        if (str[i] == '-' && i == 0)
+        {
+            s += str[i];
+            continue;
+        }
+        if (str[i] == '-')
+        {
+            if (!isdigit(str[i - 1]))
+            {
+                if (!isdigit(str[i - 2]))
+                {
+                    s += str[i];
+                    continue;
+                }
+            }
+        }
+        if (isdigit(str[i]))
+        {
+            s += str[i];
+            continue;
+        }
+        if (str[i] == '.')
+        {
+            s += str[i];
+            continue;
+        }
+        if (str[i] == '(' || str[i] == ')')
+        {
+            if (i!=0 && s!="") temp.push_back(s);
+            s = "";
+            s += str[i];
+            temp.push_back(s);
+            s = "";
         }
         else
         {
-            switch (ch)
-            {
-
-            case '(':
-                st.push(ch);
-                break;
-
-            case ')':
-                while (st.top() != '(')
-                {
-                    res += st.top();
-                    st.pop();
-                }
-                st.pop();
-                break;
-
-            case '+':
-                while (!st.empty() && (st.top() == '+' || st.top() == '-' || st.top() == '*' || st.top() == '/'))
-                {
-                    res += st.top();
-                    st.pop();
-                }
-                st.push('+');
-                break;
-
-            case '-':
-                while (!st.empty() && (st.top() == '+' || st.top() == '-' || st.top() == '*' || st.top() == '/'))
-                {
-                    res += st.top();
-                    st.pop();
-                }
-                st.push('-');
-                break;
-
-            case '*':
-                while (!st.empty() && (st.top() == '*' || st.top() == '/'))
-                {
-                    res += st.top();
-                    st.pop();
-                }
-                st.push('*');
-                break;
-
-            case '/':
-                while (!st.empty() && (st.top() == '*' || st.top() == '/'))
-                {
-                    res += st.top();
-                    st.pop();
-                }
-                st.push('/');
-                break;
-            default:
-                break;
-            }
+            if (s!="") temp.push_back(s);
+            s = "";
+            s += str[i];
+            temp.push_back(s);
+            s = "";
         }
+    }
+    if (s!="")temp.push_back(s);
+    return temp;
+}
+
+
+vector<string> toPoland(const string& str) {
+    stack<string> st;
+    vector<string> temp =strToVector(str);
+    vector<string> res(0);
+
+    for (int i = 0; i < temp.size(); i++) 
+    {               
+        if (temp[i] == "(") st.push(temp[i]);
+        else if (temp[i] == ")")
+        {
+            while (st.top() != "(")
+            {
+                res.push_back(st.top());
+                st.pop();
+            }
+            st.pop();
+        }
+        else if (temp[i] == "+")
+        {
+            while (!st.empty() && (st.top() == "+" || st.top() == "-" || st.top() == "*" || st.top() == "/"))
+            {
+                res.push_back(st.top());
+                st.pop();
+            }
+            st.push("+");
+        }
+        else if (temp[i] == "-")
+        {
+            while (!st.empty() && (st.top() == "+" || st.top() == "-" || st.top() == "*" || st.top() == "/"))
+            {
+                res.push_back(st.top());
+                st.pop();
+            }
+            st.push("-");
+        }
+        else if (temp[i] == "*")
+        {
+            while (!st.empty() && (st.top() == "*" || st.top() == "/"))
+            {
+                res.push_back(st.top());
+                st.pop();
+            }
+            st.push("*");
+        }
+        else if (temp[i] == "/")
+        {
+            while (!st.empty() && (st.top() == "*" || st.top() == "/"))
+            {
+                res.push_back(st.top());
+                st.pop();
+            }
+            st.push("/");
+        }
+        else res.push_back(temp[i]);
+        
     }
 
     while (!st.empty()) {
-        res += st.top();
+        res.push_back(st.top());
         st.pop();
     }
     return res;
 }
 
-auto operations(double a, double b, char op)
+auto operations(double a, double b, string op)
 {
-    switch (op)
-    {
-    case '+':
-        return a + b;
-    case '-':
-        return a - b;
-    case '*':
-        return a * b;
-    case '/':
-        return a / b;
-    default:
-        break;
-    }
+    
+    if (op == "+") return a + b;
+    if (op == "-") return a - b;
+    if (op == "*") return a * b;
+    if (op == "/") return a / b;
+    
 }
 double solv(const string& str)
 {
-    string polandString = toPoland(str);
-    stack<double> st;    
-    for (const char ch : polandString)
+    vector<string> polandString = toPoland(str);
+    stack<double> st;
+    for (int i=0;i<polandString.size();i++)
     {
-        if (isdigit(ch))
-        {
-            int number = ch - '0';
-            st.push(number);
-        }
-        else
+        if (polandString[i] == "(" || polandString[i] == ")" || polandString[i] == "+" || polandString[i] == "-" || polandString[i] == "*" || polandString[i] == "/")
         {
             double num2 = st.top();
             st.pop();
-            if (!st.empty())
-            {
-                double num1 = st.top();
-                st.pop();
-                st.push(operations(num1, num2, ch));
-            }
-            else
-            {
-                st.push(num2*(-1));
-            }
+            double num1 = st.top();
+            st.pop();
+            string op = polandString[i];
+            st.push(operations(num1, num2, op));
+        }
+        else
+        {            
+            double number = stof(polandString[i]);
+            st.push(number);
         }
     }
     double res = st.top();
@@ -129,7 +161,7 @@ double solv(const string& str)
 int main() {
     string str;
     cin >> str;
-    cout << solv(str);
-
+    cout << solv(str);   
+    
     return 0;
 }
